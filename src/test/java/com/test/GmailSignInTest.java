0ae466e7591dbf5f1112a3e30154de863b98cc1d
Sean <Sean>
2017-02-20 22:25:32 +0000

    Initial setup

---
+++ src/test/java/com/test/GmailSignInTest.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.junit.Test;
+
+public class GmailSignInTest {
+	@Test
+	public void gmailLoginShouldBeSuccessful(){
+//		Go to Gmail wbsite
+//		Fill in username
+//		Fill in password
+//		click sign in
+//		verify iser did sign in
+//		sign out
+//		verified user did sign out
+//		
+	}
+	
+
+}
