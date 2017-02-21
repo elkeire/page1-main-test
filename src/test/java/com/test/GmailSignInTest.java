bc69c4dc5fde1fd3b282a138024083e1161a9f48
Sean <Sean>
2017-02-21 12:59:39 +0000

    test commit

--- src/test/java/com/test/GmailSignInTest.java
+++ src/test/java/com/test/GmailSignInTest.java
@@ -1,11 +1,14 @@
 package com.test;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class GmailSignInTest {
 	@Test
 	public void gmailLoginShouldBeSuccessful(){
 //		Go to Gmail wbsite
+		WebDriver chromeDriver = new org.openqa.selenium.chrome.ChromeDriver();
+		//chromeDriver.
 //		Fill in username
 //		Fill in password
 //		click sign in
